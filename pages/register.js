675d4117ed4c73f91e606f1b31bde4d682e9aedc
sanketh sura <sanketh.sura.1@gmail.com>
2023-01-06 10:42:47 +0530

    last one

--- pages/register.js
+++ pages/register.js
@@ -37,7 +37,8 @@ const Register = (props) => {
     try {
       await createUserWithEmailAndPassword(auth, user.email, password);
       console.log("user logged in with", user.email);
-      user.setName(()=>nameRef.current.value)
+      user.setName(() => nameRef.current.value)
+      console.log(user.name)
       if (ref.current.checked) {
         localStorage.setItem("email", user.email);
         localStorage.setItem("password", password);
